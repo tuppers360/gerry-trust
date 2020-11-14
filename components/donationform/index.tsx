652259b56fd90717e4174be3e92212ec98992a3a
tuppers360 <tuppers360@live.co.uk>
2020-11-14 21:41:10 +0000

    add gitftaid and donation state

--- components/donationform/index.tsx
+++ components/donationform/index.tsx
@@ -8,8 +8,12 @@ import { fetchPostJSON } from '../../utils/api-helpers';
 import { formatAmountForDisplay } from '../../utils/stripe-helpers';
 import * as config from '../../config';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const DonationForm = () => {
+  const [donation, setDonation] = useState(0);
   const [loading, setLoading] = useState(false);
+  const [giftAid, setGiftAid] = useState(false);
   const [input, setInput] = useState({
     customDonation: Math.round(config.MAX_AMOUNT / config.AMOUNT_STEP),
   });
@@ -26,6 +30,7 @@ const DonationForm = () => {
     // Create a Checkout Session.
     const response = await fetchPostJSON('/api/stripe/checkout_sessions', {
       amount: input.customDonation,
+      giftAid: giftAid,
     });
 
     if (response.statusCode === 500) {
@@ -49,26 +54,89 @@ const DonationForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CustomDonationInput
-        className="checkout-style"
-        name={'customDonation'}
-        value={input.customDonation}
-        min={config.MIN_AMOUNT}
-        max={config.MAX_AMOUNT}
-        step={config.AMOUNT_STEP}
-        currency={config.CURRENCY}
-        onChange={handleInputChange}
-      />
-      <StripeTestCards />
-      <button
-        className="checkout-style-background"
-        type="submit"
-        disabled={loading}
-      >
-        Donate {formatAmountForDisplay(input.customDonation, config.CURRENCY)}
-      </button>
-    </form>
+    <>
+      <div className="btn-container">
+        <div className="btn item one">Flex Item 1</div>
+        <div className="btn item one">Flex Item 2</div>
+        <div className="btn item one">Flex Item 3</div>
+        <div className="btn item one">Flex Item 4</div>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <CustomDonationInput
+          className="checkout-style"
+          name={'customDonation'}
+          value={input.customDonation}
+          min={config.MIN_AMOUNT}
+          max={config.MAX_AMOUNT}
+          step={config.AMOUNT_STEP}
+          currency={config.CURRENCY}
+          onChange={handleInputChange}
+        />
+
+        <div className="giftaid">
+          <img src="/images/gift-aid-logo.png" alt="Gift Aid" />
+          <h2>Are you a UK tax payer?</h2>
+          <small>
+            Gift Aid is reclaimed by the charity from the tax you pay for the
+            current year. Your address is needed to identify you as a current UK
+            taxpayer.
+          </small>
+          <h3>
+            Boost your donation by&nbsp;
+            <strong>
+              25%&nbsp;
+              {donation > 0 && (
+                <strong>
+                  (
+                  {new Intl.NumberFormat('en-GB', {
+                    style: 'currency',
+                    currency: 'GBP',
+                  }).format(donation * 0.25)}
+                  )&nbsp;
+                </strong>
+              )}
+            </strong>
+            at no extra cost to you.
+          </h3>
+          <FontAwesomeIcon
+            id="giftAid"
+            icon={giftAid ? ['far', 'check-square'] : ['far', 'square']}
+            onClick={() => setGiftAid(!giftAid)}
+            className="checkbox-container"
+          />
+          <span>Please claim Gift Aid on my behalf</span>
+        </div>
+
+        {giftAid && (
+          <div className="declaration">
+            <p>
+              <small>
+                I confirm that this is my own money and I would like The Gerry
+                Richardson Trust to treat all the donations I have made in the
+                past 4 years (if any) and any future donations I make, unless I
+                notify you otherwise, as Gift Aid donations.
+              </small>
+            </p>
+            <p>
+              <small>
+                I also confirm that I am a UK taxpayer and understand that if I
+                pay less Income Tax and/or Capital Gains Tax in the current tax
+                year than the amount of Gift Aid claimed on all my donations it
+                is my responsibility to pay any difference.
+              </small>
+            </p>
+          </div>
+        )}
+        <StripeTestCards />
+        <button
+          className="checkout-style-background"
+          type="submit"
+          disabled={loading}
+        >
+          Donate {formatAmountForDisplay(input.customDonation, config.CURRENCY)}
+        </button>
+      </form>
+    </>
   );
 };
 

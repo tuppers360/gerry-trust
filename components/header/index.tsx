e5ed3ce5c070319b3059459e7ea8d1b033dd464b
tuppers360 <tuppers360@live.co.uk>
2020-11-16 10:03:37 +0000

    update header component to accept left and center for text

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -4,17 +4,27 @@ import styles from './header.module.scss';
 type HeaderProps = {
   title?: string;
   heading: string;
+  center?: boolean;
 };
 
 const Header: FunctionComponent<HeaderProps> = ({
   title,
   heading,
+  center,
   children,
 }) => {
   return (
     <div className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.wrap_left}>
+      <div
+        className={
+          center
+            ? `${styles.container} ${styles.center}`
+            : `${styles.container}`
+        }
+      >
+        <div
+          className={center ? `${styles.wrap_center}` : `${styles.wrap_left}`}
+        >
           <div className={styles.title}>{title}</div>
           <h1>{heading}</h1>
           {children}

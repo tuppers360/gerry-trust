1f21669e1e0e21f29461890c1023d5873f01d6ac
tuppers360 <tuppers360@live.co.uk>
2020-11-26 21:15:56 +0000

    comment out code for example

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -1,18 +1,18 @@
 import { NextApiRequest, NextApiResponse  } from 'next';
 
-import { sendEmail } from '../../utils/sendEmail';
+// import { sendEmail } from '../../utils/sendEmail';
 
-//TODO - amend api files to typescript - EXAMPLE
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if(req.method === 'POST') {
-      const { name, email } = req.body;
-      await sendEmail({ name, email });
-      return res.status(200).end();
-    }
-    return res.status(404).json({
-        error: {
-            code: 'not_found',
-            messgae: "The requested endpoint was not found or doesn't support this method."
-        }
-    });
-}
+// //TODO - amend api files to typescript - EXAMPLE
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//     if(req.method === 'POST') {
+//       const { name, email } = req.body;
+//       await sendEmail({ name, email });
+//       return res.status(200).end();
+//     }
+//     return res.status(404).json({
+//         error: {
+//             code: 'not_found',
+//             messgae: "The requested endpoint was not found or doesn't support this method."
+//         }
+//     });
+// }

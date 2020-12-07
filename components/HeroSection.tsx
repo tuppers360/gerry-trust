cb5bdd7e568ce57a8c5c14bab3849ac848fbb159
tuppers360 <tuppers360@live.co.uk>
2020-12-07 10:56:53 +0000

    add hero section

---
+++ components/HeroSection.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function HeroSection() {
+  return (
+    <div className="bg-gray-200">
+      <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+        <h2 className="text-5xl tracking-tight font-extrabold text-gray-800 sm:text-6xl">
+          Our Mission
+        </h2>
+        <p className="mt-4 text-lg leading-6 text-gray-800">
+          The trust seeks to promote youth development by supporting young
+          people, aged 25 or under, to attend courses and activities of an
+          educational, cultural, sporting, adventuresome or character-building
+          nature. It is willing to entertain written applications for financial
+          assistance from young people living or working within 15 miles of the
+          town hall in Blackpool, Lancashire.
+        </p>
+      </div>
+    </div>
+  );
+}

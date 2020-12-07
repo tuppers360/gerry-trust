c7c1ac841d801aa86e98a2113075f5dca8b9d36c
tuppers360 <tuppers360@live.co.uk>
2020-12-07 10:55:27 +0000

    add navigation bar

---
+++ components/tailwindnav/index.tsx
@@ -0,0 +1,145 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+
+export default function TailwindNav() {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log('isOpen', isOpen);
+
+  return (
+    // <!-- This example requires Tailwind CSS v2.0+ -->
+    <nav className="bg-white shadow">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex">
+            <div className="-ml-2 mr-2 flex items-center md:hidden">
+              {/* <!-- Mobile menu button --> */}
+              <button
+                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {/* <!-- Icon when menu is closed. --> */}
+                {/* <!--
+                  Heroicon name: menu
+                  Menu open: "hidden", Menu closed: "block"
+                --> */}
+                <svg
+                  className={`h-6 w-6 ${isOpen ? 'hidden' : 'block'}`}
+                  onClick={() => setIsOpen(!isOpen)}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+                {/* <!-- Icon when menu is open. --> */}
+                {/* <!--
+                  Heroicon name: x
+                  Menu open: "block", Menu closed: "hidden"
+                --> */}
+                <svg
+                  className={`h-6 w-6 ${isOpen ? 'block' : 'hidden'}`}
+                  onClick={() => setIsOpen(!isOpen)}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="flex-shrink-0 flex items-center">
+              <img
+                className="block lg:hidden h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt="Workflow"
+              />
+              <img
+                className="hidden lg:block h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+                alt="Workflow"
+              />
+            </div>
+            <div className="hidden md:ml-6 md:flex md:space-x-8">
+              {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
+              <Link href="/stories">
+                <a className="border-blue-800 text-gray-800 inline-flex items-center px-1 pt-1 border-b-4 text-md font-semibold">
+                  Stories
+                </a>
+              </Link>
+              <Link href="#">
+                <a className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-4 text-md font-semibold">
+                  News
+                </a>
+              </Link>
+              <Link href="/about">
+                <a className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-4 text-md font-semibold">
+                  About
+                </a>
+              </Link>
+              <Link href="/contact">
+                <a className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-4 text-md font-semibold">
+                  Contact Us
+                </a>
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <Link href="/donate">
+              <a className="bg-blue-900 text-white hover:bg-blue-800 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1">
+                <FontAwesomeIcon className="mr-1" icon="gift" fixedWidth />
+                Donate
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* <!--
+        Mobile menu, toggle classes based on menu state.
+    
+        Menu open: "block", Menu closed: "hidden"
+      --> */}
+      <div className={`md:hidden ${isOpen ? 'block' : 'hidden'}`}>
+        <div className="pt-2 pb-3 space-y-1">
+          {/* <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" --> */}
+          <Link href="/stories">
+            <a className="bg-indigo-50 border-blue-800 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6">
+              Stories
+            </a>
+          </Link>
+          <Link href="#">
+            <a className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6">
+              News
+            </a>
+          </Link>
+          <Link href="/about">
+            <a className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6">
+              About
+            </a>
+          </Link>
+          <Link href="contact">
+            <a className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6">
+              Contact Us
+            </a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}

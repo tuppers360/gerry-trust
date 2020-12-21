b74829668dade07db1eb8198dc983928f0c870b6
tuppers360 <tuppers360@live.co.uk>
2020-12-21 15:58:07 +0000

    apply tailwind styles and update page to be cleaner code

--- components/DonationForm.tsx
+++ components/DonationForm.tsx
@@ -6,29 +6,21 @@ import { formatAmountForDisplay } from '../utils/stripe-helpers';
 import * as config from '../config';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import GiftAid from './GiftAid';
 
 const DonationForm = () => {
   const [donation, setDonation] = useState(0);
   const [showCustomDonation, setShowCustomDonation] = useState(false);
   const [loading, setLoading] = useState(false);
   const [giftAid, setGiftAid] = useState(false);
-  const [input, setInput] = useState({
-    customDonation: Math.round(config.MAX_AMOUNT / config.AMOUNT_STEP),
-  });
   const [activeButton, setActiveButton] = useState(0);
-  const [activeButtonAmountType, setActiveButtonAmountType] = useState(1);
+  //const [activeButtonAmountType, setActiveButtonAmountType] = useState(1);
   const buttonArray = [
-    { id: 1, text: '5' },
-    { id: 2, text: '10' },
-    { id: 3, text: '20' },
-    { id: 4, text: 'Other' },
+    { id: 1, value: 5, text: '£5' },
+    { id: 2, value: 10, text: '£10' },
+    { id: 3, value: 20, text: '£20' },
+    { id: 4, value: 0, text: 'Other' },
   ];
-
-  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = (e) =>
-    setInput({
-      ...input,
-      [e.currentTarget.name]: e.currentTarget.value,
-    });
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
@@ -71,149 +63,87 @@ const DonationForm = () => {
     }
   };
 
+  const handleGiftAid = () => {
+    setGiftAid(!giftAid);
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <div>
-          <ul
-            className="btn-group-donation-type"
-            aria-label="Donation Button Group Type"
-          >
-            <li key="1">
-              <span
-                className={`btn btn_depth btn${
-                  activeButtonAmountType === 1 ? '_warning' : '_primary'
+        <div className="grid grid-cols-4 gap-x-1 gap-y-2">
+          <div className="bg-blue-800 text-gray-100 shadow-sm py-4 px-2 rounded text-base text-center flex justify-center items-center cursor-pointer col-span-2">
+            One-Off Payment
+          </div>
+          <div className="border-2 border-solid border-blue-800 shadow-sm text-gray-800 py-4 px-2 rounded text-base text-center flex items-center justify-center col-span-2 cursor-auto">
+            Monthly Donation (coming soon)
+          </div>
+          {buttonArray.map((button) => {
+            return (
+              <div
+                key={button.id}
+                className={`transition-colors duration-300 easr-in-out py-2 rounded text-lg font-semibold flex justify-center items-center cursor-pointer hover:bg-blue-800 hover:text-gray-100 focus:outline-none ${
+                  activeButton === button.id
+                    ? 'bg-blue-800 text-gray-100'
+                    : 'border-2 border-solid border-blue-800 shadow-sm text-gray-700'
                 }`}
+                onClick={() => toggle(button.id, button.value)}
               >
-                One Off Payment
-              </span>
-            </li>
-            <li key="2">
-              <span className="btn btn_depth btn_primary disabled">
-                Regular Payment (coming Soon)
-              </span>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <ul
-            className="btn-group-donate-buttons"
-            aria-label="Donation Button Group Amount"
-          >
-            {buttonArray.map((button) => {
-              return (
-                <li key={button.id}>
-                  <span
-                    className={`btn btn_depth btn${
-                      activeButton === button.id ? '_warning' : '_primary'
-                    }`}
-                    onClick={() => toggle(button.id, button.text)}
-                  >
-                    {button.id != 4 ? (
-                      <div>
-                        {formatAmountForDisplay(
-                          parseFloat(button.text),
-                          config.CURRENCY
-                        )}
-                      </div>
-                    ) : (
-                      <div>{button.text}</div>
-                    )}
-                  </span>
-                </li>
-              );
-            })}
-          </ul>
+                {button.text}
+              </div>
+            );
+          })}
         </div>
         {showCustomDonation && (
-          <div className="form_item customdonation-amount">
-            <label htmlFor="customdonationamount">Donate what you want:</label>
-            <input
-              id="customdonationamount"
-              pattern="[0-9]*"
-              name="donation"
-              type="number"
-              aria-describedby="Custom Donation Amount"
-              placeholder="Enter Donation"
-              min="1"
-              onChange={(e) => setDonation(parseFloat(e.currentTarget.value))}
-              className="form_input text_center"
-            />
-          </div>
-        )}
-        <div className="giftaid">
-          <img src="/images/gift-aid-logo.png" alt="Gift Aid" />
-          <h2>Are you a UK tax payer?</h2>
-          <p>
-            Gift Aid is reclaimed by the charity from the tax you pay for the
-            current year. Your address is needed to identify you as a current UK
-            taxpayer.
-          </p>
-          <p>
-            Boost your donation by&nbsp;
-            <strong>
-              25%&nbsp;
-              {donation > 0 && (
-                <span>
-                  (
-                  {new Intl.NumberFormat('en-GB', {
-                    style: 'currency',
-                    currency: 'GBP',
-                  }).format(donation * 0.25)}
-                  )&nbsp;
+          <div className="sm:grid sm:grid-cols-4 sm:gap-4 sm:items-start max-w-xl mx-auto mt-4">
+            <label
+              htmlFor="donation"
+              className="block text-lg font-semibold text-center text-gray-600 sm:pt-2 sm:col-span-2 sm:text-left"
+            >
+              Donate what you want:
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <div className="max-w-lg flex rounded-md shadow-sm">
+                <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
+                  <FontAwesomeIcon icon="pound-sign" />
                 </span>
-              )}
-            </strong>
-            at no extra cost to you.
-          </p>
-          <FontAwesomeIcon
-            id="giftAid"
-            icon={giftAid ? ['far', 'check-square'] : ['far', 'square']}
-            onClick={() => setGiftAid(!giftAid)}
-            className="checkbox-container"
-          />
-          <span>Please claim Gift Aid on my behalf</span>
-        </div>
-
-        {giftAid && (
-          <div className="declaration">
-            <p>
-              <small>
-                I confirm that this is my own money and I would like The Gerry
-                Richardson Trust to treat all the donations I have made in the
-                past 4 years (if any) and any future donations I make, unless I
-                notify you otherwise, as Gift Aid donations.
-              </small>
-            </p>
-            <p>
-              <small>
-                I also confirm that I am a UK taxpayer and understand that if I
-                pay less Income Tax and/or Capital Gains Tax in the current tax
-                year than the amount of Gift Aid claimed on all my donations it
-                is my responsibility to pay any difference.
-              </small>
-            </p>
+                <input
+                  id="customdonationamount"
+                  pattern="[0-9]*"
+                  name="donation"
+                  type="number"
+                  aria-describedby="Custom Donation Amount"
+                  placeholder="Enter Donation"
+                  min="1"
+                  onChange={(e) =>
+                    setDonation(parseFloat(e.currentTarget.value))
+                  }
+                  className="flex-1 block w-full focus:ring-indigo-500 focus:border-blue-800 min-w-0 rounded-none rounded-r-md text-center sm:text-sm border-gray-300"
+                />
+              </div>
+            </div>
           </div>
         )}
-        <button
-          className={`btn btn_primary full-width donate ${
-            donation >= 0.3 ? '' : 'disabled'
-          }`}
-          type="submit"
-          disabled={loading}
-        >
-          {loading && (
-            <span className="icon_margin_right">
-              <FontAwesomeIcon icon="sync" spin />
-            </span>
-          )}
-          &nbsp;Donate&nbsp;
-          {donation >= config.MIN_AMOUNT && (
-            <span className="currency-icon">
-              {formatAmountForDisplay(donation, config.CURRENCY)}
-            </span>
-          )}
-        </button>
+        <GiftAid
+          handleGiftAid={handleGiftAid}
+          donation={donation}
+          giftaid={giftAid}
+        />
+        <div className="flex items-center justify-center">
+          <button
+            className={`inline-flex items-center mt-5 justify-center px-8 py-3 mb-1 border border-transparent text-base font-semibold rounded-md text-white bg-blue-900 hover:bg-blue-800 md:py-4 md:px-24  ${
+              donation >= 1 ? '' : 'disabled'
+            }`}
+            type="submit"
+            disabled={loading || donation === 0}
+          >
+            {loading && (
+              <span className="mr-2">
+                <FontAwesomeIcon icon="sync" spin />
+              </span>
+            )}
+            Proceed to payment
+          </button>
+        </div>
       </form>
     </>
   );

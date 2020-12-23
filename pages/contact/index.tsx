ee34da657a0e790b5701b0fbd3a987b83fec3cde
tuppers360 <tuppers360@live.co.uk>
2020-12-23 06:42:03 +0000

    remove css and add tailwind styles

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ContactForm from '../../components/contact-form';
+import ContactForm from '../../components/ContactForm';
 import PageHeaderSection from '../../components/PageHeaderSection';
 import Layout from '../../components/Layout';
-import styles from './contact.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ContactFormTest from '../../components/ContactFormTest';
 
 function Contact() {
   return (
@@ -15,18 +15,18 @@ function Contact() {
         >
           <p>Below you will find a few ways to contact us</p>
         </PageHeaderSection>
-        <div className={styles.content_section}>
-          <div className={styles.container}>
-            <div className={`${styles.contact_grid} ${styles.layout_grid}`}>
-              <div className={styles.column}>
+        {/* <div className="">
+          <div className="">
+            <div className="">
+              <div className="">
                 <h3>Send us a message</h3>
                 <div>
                   <ContactForm />
                 </div>
               </div>
-              <div className={styles.column}>
+              <div className="">
                 <h3>
-                  <span className={styles.margin}>
+                  <span className="">
                     <FontAwesomeIcon icon="home" fixedWidth />
                   </span>
                   Get in touch
@@ -50,6 +50,38 @@ function Contact() {
                   FY6 9AF
                 </p>
               </div>
+            </div>
+          </div>
+        </div> */}
+        <div className="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-6xl lg:px-8 m-12">
+          <div className="grid lg:grid-cols-2">
+            <ContactFormTest />
+
+            <div className="">
+              <h3>
+                <span className="">
+                  <FontAwesomeIcon icon="home" fixedWidth />
+                </span>
+                Get in touch
+              </h3>
+              <p>
+                We’re very approachable and would love to speak to you. Feel
+                free to call, send us an email, Tweet us or simply complete the
+                enquiry form.
+              </p>
+              <p>
+                <strong>Gerry Richardson Trust</strong>
+                <br />
+                Northdene,
+                <br />
+                Stoney Lane,
+                <br />
+                Hambleton,
+                <br />
+                Poulton-Le-Fylde,
+                <br />
+                FY6 9AF
+              </p>
             </div>
           </div>
         </div>

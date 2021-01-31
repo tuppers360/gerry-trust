4c92430b6cc05414c3d4ce1bf5dd910ae03dd207
tuppers360 <tuppers360@live.co.uk>
2021-01-31 09:40:11 +0000

    add MDX component for provider

---
+++ components/MDXComponents.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+//TODO - build these as we move along adding the Blog etc
+// import Tweet from 'react-tweet-embed';
+
+// import ProsCard from '@/components/ProsCard';
+// import ConsCard from '@/components/ConsCard';
+// import Gumroad from '@/components/metrics/Gumroad';
+// import Unsplash from '@/components/metrics/Unsplash';
+// import Analytics from '@/components/metrics/Analytics';
+// import YouTube from '@/components/metrics/Youtube';
+// import Step from '@/components/Step';
+
+const CustomLink = (props) => {
+  const href = props.href;
+  const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
+
+  if (isInternalLink) {
+    return (
+      <Link href={href}>
+        <a {...props} />
+      </Link>
+    );
+  }
+
+  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+};
+
+const MDXComponents = {
+  Image,
+  a: CustomLink
+
+  //TODO - as above build these as we add the blog
+  //   Analytics,
+  //   ConsCard,
+  //   Gumroad,
+  //   ProsCard,
+  //   Step,
+  //   Tweet,
+  //   Unsplash,
+  //   YouTube
+};
+
+export default MDXComponents;

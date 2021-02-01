11277d08ff53747cc130e15e173813d440fbf458
tuppers360 <tuppers360@live.co.uk>
2021-02-01 21:43:02 +0000

    add blog layout

---
+++ layouts/blog.tsx
@@ -0,0 +1,75 @@
+import { format, parseISO } from 'date-fns';
+
+import Container from 'components/Container';
+import Image from 'next/image';
+
+//TODO - add these components later
+// import Subscribe from '@/components/Subscribe';
+// import ViewCounter from '@/components/ViewCounter';
+
+const editUrl = (slug) =>
+  `https://github.com/leerob/leerob.io/edit/master/data/blog/${slug}.mdx`;
+const discussUrl = (slug) =>
+  `https://mobile.twitter.com/search?q=${encodeURIComponent(
+    `https://leerob.io/blog/${slug}`
+  )}`;
+
+export default function BlogLayout({ children, frontMatter }) {
+  return (
+    <Container
+      title={`${frontMatter.title} – Lee Robinson`}
+      description={frontMatter.summary}
+      image={`https://http://localhost:3000/${frontMatter.image}`}
+      date={new Date(frontMatter.publishedAt).toISOString()}
+      type="article"
+    >
+      <article className="flex flex-col justify-center items-start max-w-xl mx-auto px-4 sm:px-6 lg:max-w-5xl lg:px-8 m-4">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+          {frontMatter.title}
+        </h1>
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2 mb-8">
+          <div className="flex items-center">
+            <Image
+              alt="Lee Robinson"
+              height={24}
+              width={24}
+              src="/avatar.jpg"
+              className="rounded-full"
+            />
+            <p className="text-sm text-gray-700 dark:text-gray-300 ml-2">
+              {frontMatter.by}
+              {'Lee Robinson / '}
+              {format(parseISO(frontMatter.publishedAt), 'MMMM dd, yyyy')}
+            </p>
+          </div>
+          <p className="text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
+            {frontMatter.readingTime.text}
+            {` • `}
+            {/* <ViewCounter slug={frontMatter.slug} /> */}
+          </p>
+        </div>
+        <div className="prose dark:prose-dark max-w-none w-full">
+          {children}
+        </div>
+        <div className="mt-8">{/* <Subscribe /> */}</div>
+        <div className="text-sm text-gray-700 dark:text-gray-300">
+          <a
+            href={discussUrl(frontMatter.slug)}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {'Discuss on Twitter'}
+          </a>
+          {` • `}
+          <a
+            href={editUrl(frontMatter.slug)}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {'Edit on GitHub'}
+          </a>
+        </div>
+      </article>
+    </Container>
+  );
+}

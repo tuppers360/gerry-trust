c3f8b7df0775742d0886a666e649856a8a227dc4
tuppers360 <tuppers360@live.co.uk>
2021-02-01 21:44:05 +0000

    add blog slug route

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,32 @@
+import { getFileBySlug, getFiles } from 'lib/mdx';
+
+import BlogLayout from 'layouts/blog';
+import MDXComponents from 'components/MDXComponents';
+import hydrate from 'next-mdx-remote/hydrate';
+
+export default function Blog({ mdxSource, frontMatter }) {
+  const content = hydrate(mdxSource, {
+    components: MDXComponents
+  });
+
+  return <BlogLayout frontMatter={frontMatter}>{content}</BlogLayout>;
+}
+
+export async function getStaticPaths() {
+  const posts = await getFiles('blog');
+
+  return {
+    paths: posts.map((p) => ({
+      params: {
+        slug: p.replace(/\.mdx/, '')
+      }
+    })),
+    fallback: false
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const post = await getFileBySlug('blog', params.slug);
+
+  return { props: post };
+}

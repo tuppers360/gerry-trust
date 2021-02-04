f9558050bc9295a8af6d56f836630998976f03d2
tuppers360 <tuppers360@live.co.uk>
2021-02-04 12:31:27 +0000

    add custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,25 @@
+import Container from 'components/Container';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <Container title="404 – Gerry Richardson Trust">
+      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mt-24">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-grey-800 text-center dark:text-white">
+          451 – Unavailable For Legal Reasons
+        </h1>
+        <p className="text-gray-600 dark:text-gray-400 mb-8">
+          Why show a generic 404 when I can make it sound mysterious? It seems
+          you've found something that used to exist, or you spelled something
+          wrong. I'm guessing you spelled something wrong. Can you double check
+          that URL?
+        </p>
+        <Link href="/">
+          <a className="p-1 sm:p-4 w-64 font-bold mx-auto bg-gray-100 dark:bg-gray-900 text-center rounded-md text-grey-800 dark:text-grey-100 shadow-lg border-gray-900">
+            Return Home
+          </a>
+        </Link>
+      </div>
+    </Container>
+  );
+}

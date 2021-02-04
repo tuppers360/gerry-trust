7af83f228b59cc366bcf1f483628665abeaf8b59
tuppers360 <tuppers360@live.co.uk>
2021-02-04 22:10:57 +0000

    make heading required and amend styles

--- components/PageHeaderSection.tsx
+++ components/PageHeaderSection.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 
 type HeaderProps = {
   title?: string;
-  heading?: string;
+  heading: string;
   type?: string;
   center?: boolean;
   author?: string;
@@ -23,13 +23,13 @@ const PageHeaderSection: FunctionComponent<HeaderProps> = ({
           </p>
         )}
         <h1
-          className={`bg-clip-text text-transparent bg-gradient-to-r from-blue-900 to-blue-500 font-bold py-4${
-            type === 'story' ? ' text-7xl font-julietta' : ' text-6xl'
+          className={`bg-clip-text text-transparent bg-gradient-to-r from-blue-900 to-blue-500 font-bold py-4 ${
+            type === 'story' ? ' font-julietta text-7xl' : ' text-6xl'
           }`}
         >
           {heading}
         </h1>
-        <div className="mt-6 text-xl font-bold">{children}</div>
+        {children && <div className="mt-6 text-xl font-bold">{children}</div>}
       </div>
     </div>
   );

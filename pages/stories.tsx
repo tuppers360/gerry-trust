11c2e902fe5603eb074d0e05129b99898091ce23
tuppers360 <tuppers360@live.co.uk>
2021-02-21 17:59:01 +0000

    amend grig layout for stories

--- pages/stories.tsx
+++ pages/stories.tsx
@@ -51,7 +51,7 @@ function Stories({ stories }) {
         {!filteredBlogPosts.length && 'No posts found.'}
         {filteredBlogPosts && (
           <div className="relative">
-            <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
+            <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-2 xl:grid-cols-3 lg:max-w-7xl">
               {filteredBlogPosts.map((frontMatter) => (
                 <StoryCard key={frontMatter.title} {...frontMatter} />
               ))}

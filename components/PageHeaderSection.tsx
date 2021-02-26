e53c982ec5716293ee54eca7a2c56c4e204813c9
tuppers360 <tuppers360@live.co.uk>
2021-02-26 22:56:35 +0000

    remove icon for views and format docs

--- components/PageHeaderSection.tsx
+++ components/PageHeaderSection.tsx
@@ -16,17 +16,17 @@ const PageHeaderSection: FunctionComponent<HeaderProps> = ({
 }) => {
   return (
     <div className="pt-12 pb-12">
-      <div className="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-3xl">
+      <div className="max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-3xl">
         {title && (
-          <p className="text-xl font-semibold tracking-wider text-cyan-600 uppercase">
+          <p className="text-xl font-semibold tracking-wider uppercase text-cyan-600">
             {title}
           </p>
         )}
-        <h1 className="mt-4 text-3xl font-extrabold text-gray-700 tracking-tight sm:text-5xl">
+        <h1 className="mt-4 text-3xl font-extrabold tracking-tight text-gray-700 sm:text-5xl">
           {heading}
         </h1>
         {children && (
-          <div className="mt-8 mx-auto max-w-prose text-xl text-gray-500">
+          <div className="mx-auto mt-8 text-xl text-gray-500 max-w-prose">
             {children}
           </div>
         )}
--- components/StoryCard.tsx
+++ components/StoryCard.tsx
@@ -12,39 +12,24 @@ const Post = ({ title, summary, publishedAt, author, image, slug }) => {
 
   return (
     <Link href={`/stories/${slug}`}>
-      <a className="flex flex-col rounded-lg shadow-lg overflow-hidden group">
+      <a className="flex flex-col overflow-hidden rounded-lg shadow-lg group">
         <div className="flex-shrink-0">
           <img
-            className="h-48 lg:h-72 w-full object-cover"
+            className="object-cover w-full h-48 lg:h-72"
             src={image}
             alt=""
           />
         </div>
-        <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+        <div className="flex flex-col justify-between flex-1 p-6 bg-white">
           <div className="flex-1">
             <p className="text-sm font-medium text-cyan-600">Story</p>
-            <p className="text-xl font-semibold text-gray-700 mt-2 group-hover:underline">
+            <p className="mt-2 text-xl font-semibold text-gray-700 group-hover:underline">
               {title}
             </p>
             <p className="mt-3 text-base text-gray-500">{summary}</p>
           </div>
-          <div className="mt-8 flex text-sm justify-between text-gray-400">
+          <div className="flex justify-between mt-8 text-sm text-gray-400">
             <div className="inline-flex items-center">
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="far"
-                data-icon="eye"
-                className="inline-block w-5 h-5 mr-2"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 576 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M288 144a110.94 110.94 0 0 0-31.24 5 55.4 55.4 0 0 1 7.24 27 56 56 0 0 1-56 56 55.4 55.4 0 0 1-27-7.24A111.71 111.71 0 1 0 288 144zm284.52 97.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400c-98.65 0-189.09-55-237.93-144C98.91 167 189.34 112 288 112s189.09 55 237.93 144C477.1 345 386.66 400 288 400z"
-                ></path>
-              </svg>
               {`${views ? format(views) : '–––'} views`}
             </div>
             <div>
@@ -55,51 +40,6 @@ const Post = ({ title, summary, publishedAt, author, image, slug }) => {
             </div>
           </div>
         </div>
-
-        {/* <div classNameName="flex-shrink-0">
-          <img
-            classNameName="h-56 lg:h-72 w-full object-cover"
-            src={image}
-            alt=""
-          />
-        </div>
-        <div classNameName="flex-1 bg-gray-50 px-6 py-3 flex flex-col justify-between"> 
-          <div classNameName="h-0 flex-1">
-            <p classNameName="sm:text-lg sm:leading-snug font-semibold tracking-wide text-blue-600 mt-4">
-              Written by {author}
-            </p>
-            <h1 classNameName="text-3xl sm:text-4xl lg:text-5xl leading-relaxed font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-900 to-blue-500 mt-4 hover:underline">
-              {title}
-            </h1>
-            <p classNameName="mt-4 text-lg font-medium text-gray-500">{summary}</p>
-          </div>
-          <div classNameName="mt-8 flex text-sm justify-between text-gray-400">
-            <div classNameName="inline-flex items-center">
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="far"
-                data-icon="eye"
-                classNameName="inline-block w-5 h-5 mr-2"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 576 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M288 144a110.94 110.94 0 0 0-31.24 5 55.4 55.4 0 0 1 7.24 27 56 56 0 0 1-56 56 55.4 55.4 0 0 1-27-7.24A111.71 111.71 0 1 0 288 144zm284.52 97.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400c-98.65 0-189.09-55-237.93-144C98.91 167 189.34 112 288 112s189.09 55 237.93 144C477.1 345 386.66 400 288 400z"
-                ></path>
-              </svg>
-              <ViewCounter slug={slug} />
-            </div>
-            <div>
-              Published -
-              <span classNameName="ml-1">
-                {format(parseISO(publishedAt), 'MMMM dd, yyyy')}
-              </span>
-            </div>
-          </div>
-        </div>*/}
       </a>
     </Link>
   );

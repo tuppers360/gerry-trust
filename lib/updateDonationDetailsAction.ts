a13c87252315db7b8b8fd4ecbc4f54c523eca1ad
tuppers360 <tuppers360@live.co.uk>
2022-06-06 21:19:57 +0100

    amend properties to be optional

--- lib/updateDonationDetailsAction.ts
+++ lib/updateDonationDetailsAction.ts
@@ -3,16 +3,16 @@ import { GlobalState } from 'little-state-machine';
 export default function updateDonationDetailsAction(
   state: GlobalState,
   payload: {
-    firstName: string;
-    lastName: string;
-    email: string;
-    addressLine1: string;
-    addressLine2: string;
-    town: string;
-    county: string;
-    postCode: string;
-    amount: number;
-    giftAid: boolean;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    addressLine1?: string;
+    addressLine2?: string;
+    town?: string;
+    county?: string;
+    postCode?: string;
+    amount?: number;
+    giftAid?: boolean;
   }
 ) {
   console.log('state:', state);

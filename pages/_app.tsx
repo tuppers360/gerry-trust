aeba1cca9bca79cbb0e6c1fa32043d2a5bf9fd67
tuppers360 <tuppers360@live.co.uk>
2022-06-06 22:02:30 +0100

    remove font awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,9 @@
 import 'styles/tailwind.css';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-
-import {
-  faBars,
-  faCoffee,
-  faCopyright,
-  faEdit,
-  faEnvelope,
-  faFingerprint,
-  faGift,
-  faHome,
-  faPaperPlane,
-  faPhone,
-  faPoundSign,
-  faSync
-} from '@fortawesome/free-solid-svg-icons';
 
 import type { AppProps } from 'next/app';
 import MDXComponents from 'components/MDXComponents';
 import { MDXProvider } from '@mdx-js/react';
 import { ThemeProvider } from 'next-themes';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
-
-library.add(
-  fab,
-  far,
-  faBars,
-  faCoffee,
-  faCopyright,
-  faEdit,
-  faEnvelope,
-  faFingerprint,
-  faGift,
-  faHome,
-  faPaperPlane,
-  faPoundSign,
-  faPhone,
-  faSync
-);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

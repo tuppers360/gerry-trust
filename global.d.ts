8519b4848c7e514636924afd6901a86300b63038
tuppers360 <tuppers360@live.co.uk>
2022-06-06 20:51:29 +0100

    make fields optional in globalstate

--- global.d.ts
+++ global.d.ts
@@ -3,16 +3,16 @@ import 'little-state-machine';
 declare module 'little-state-machine' {
   interface GlobalState {
     donationDetails: {
-      firstName: string;
-      lastName: string;
-      email: string;
-      addressLine1: string;
-      addressLine2: string;
-      town: string;
-      county: string;
-      postCode: string;
-      amount: number;
-      giftAid: boolean;
+      firstName?: string;
+      lastName?: string;
+      email?: string;
+      addressLine1?: string;
+      addressLine2?: string;
+      town?: string;
+      county?: string;
+      postCode?: string;
+      amount?: number;
+      giftAid?: boolean;
     };
   }
 }

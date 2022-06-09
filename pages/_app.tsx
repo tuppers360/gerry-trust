9a127e59e4b3f5a79ad971c7700ce55e9c7f0a4b
tuppers360 <tuppers360@live.co.uk>
2022-06-09 10:09:20 +0100

    move little-state-machine to _app.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,37 @@
 import 'styles/tailwind.css';
 
+import { StateMachineProvider, createStore } from 'little-state-machine';
+
 import type { AppProps } from 'next/app';
 import MDXComponents from 'components/MDXComponents';
 import { MDXProvider } from '@mdx-js/react';
 import { ThemeProvider } from 'next-themes';
 
+// createStore({})
+createStore({
+  donationDetails: {
+    firstName: '',
+    lastName: '',
+    email: '',
+    addressLine1: '',
+    addressLine2: '',
+    town: '',
+    county: '',
+    postCode: '',
+    amount: 0,
+    giftAid: false
+  }
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <MDXProvider components={MDXComponents}>
-        <Component {...pageProps} />
-      </MDXProvider>
-    </ThemeProvider>
+    <StateMachineProvider>
+      <ThemeProvider attribute="class">
+        <MDXProvider components={MDXComponents}>
+          <Component {...pageProps} />
+        </MDXProvider>
+      </ThemeProvider>
+    </StateMachineProvider>
   );
 }
 

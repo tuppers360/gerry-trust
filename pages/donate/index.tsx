d0b38dd1ab7ae053970b8b96f083344b120aadf3
tuppers360 <tuppers360@live.co.uk>
2022-06-09 14:49:13 +0100

    remove unused code

--- pages/donate/index.tsx
+++ pages/donate/index.tsx
@@ -4,27 +4,21 @@ import DonationFormStep2 from 'components/donation/DonationFormStep2';
 import { NextPage } from 'next';
 import PageHeaderSection from 'components/PageHeaderSection';
 import { useState } from 'react';
-import { useRouter } from 'next/router';
 
 const DonatePage: NextPage = () => {
   const [step, setStep] = useState(0);
-  const router = useRouter();
-  const formStep = router.query.step ?? 0;
 
-  const goToStep = (step, asPath) => {
-    router.push(`/?step=${step}`, asPath);
-  };
   return (
     <Container title="Make a Donation - The Gerry Richardson Trust">
-      <PageHeaderSection title="Donate" heading="Make a Donation" center>
+      <PageHeaderSection title="Donate" heading="Make a Donation">
         <p>
           Donate to help us to help the local youths of Blackpool, Fylde and
           Wyre 💖
         </p>
       </PageHeaderSection>
       <div className="max-w-4xl px-4 mx-auto">
-        {step >= 0 && <DonationFormStep1 step={step} setStep={setStep} />}
-        {step >= 1 && <DonationFormStep2 step={step} setStep={setStep} />}
+        {step === 0 && <DonationFormStep1 step={step} setStep={setStep} />}
+        {step === 1 && <DonationFormStep2 step={step} setStep={setStep} />}
       </div>
     </Container>
   );

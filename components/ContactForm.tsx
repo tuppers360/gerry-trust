333e7145e8c92294ee058849372db35f40613b29
tuppers360 <tuppers360@live.co.uk>
2022-06-09 13:00:02 +0100

    refactor form Input fields

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,11 +1,22 @@
+import * as yup from 'yup';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import FormErrorIcon from './FormErrorIcon';
 import ToggleButton from './ToggleButton'; //Commented out until Privacy and Cookies Policy written
 import { XCircleIcon } from '@heroicons/react/solid';
 import { faSync } from '@fortawesome/free-solid-svg-icons';
-import { useForm } from 'react-hook-form';
+import { Path, useForm, UseFormRegister } from 'react-hook-form';
 import { useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
 
+const schema = yup.object({
+  firstName: yup.string().required('Please enter your first name'),
+  lastName: yup.string().required('Please enter your last name'),
+  email: yup
+    .string()
+    .required('Please enter your email address')
+    .email('Please enter a valid email address'),
+  message: yup.string().required('Please enter your message')
+});
 export interface IStatus {
   submitted?: boolean;
   submitting?: boolean;
@@ -15,12 +26,20 @@ export interface IStatus {
   };
 }
 
-type FormInputs = {
+type InputProps = {
+  label: Path<IFormValues>;
+  register: UseFormRegister<IFormValues>;
+  type: string;
+  placeholder: string;
+  labelText: string;
+};
+
+interface IFormValues {
   firstName: string;
   lastName: string;
   email: string;
   message: string;
-};
+}
 
 export default function ContactFormTest() {
   const [status, setStatus] = useState<IStatus>({
@@ -32,9 +51,43 @@ export default function ContactFormTest() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
     reset
-  } = useForm<FormInputs>();
+  } = useForm<IFormValues>({ resolver: yupResolver(schema) });
+
+  const Input = ({
+    label,
+    type,
+    register,
+    placeholder,
+    labelText
+  }: InputProps) => {
+    return (
+      <>
+        <label className="block text-sm font-medium text-gray-700">
+          {labelText}
+        </label>
+        <div className="relative mt-1">
+          <input
+            className={`py-3 px-4 block w-full shadow-sm rounded-md sm:text-sm ${
+              errors[label]
+                ? `pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500`
+                : 'focus:ring-blue-900 focus:border-blue-900 border-gray-300'
+            }`}
+            type={type}
+            {...register(label)}
+            placeholder={placeholder}
+          />
+          {errors[label] && <FormErrorIcon />}
+        </div>
+        {errors[label] && (
+          <p className="mt-2 text-sm text-red-600" id="email-error">
+            <span>{errors[label].message}</span>
+          </p>
+        )}
+      </>
+    );
+  };
 
   const [submittedData, setSubmittedData] = useState({});
 
@@ -54,11 +107,12 @@ export default function ContactFormTest() {
   };
 
   const handleOnSubmit = async (
-    data: FormInputs,
+    data: IFormValues,
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
     e.preventDefault();
     setSubmittedData(data);
+    console.log('DATA:', data);
     setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
     const res = await fetch('/api/sendgrid/contactus', {
       method: 'POST',
@@ -78,7 +132,7 @@ export default function ContactFormTest() {
           <div className="p-4 bg-red-100 border-l-4 border-red-400 rounded-md">
             <div className="flex">
               <div className="flex-shrink-0 mt-1">
-                <XCircleIcon className="h-5 w-5 text-red-500" />
+                <XCircleIcon className="w-5 h-5 text-red-500" />
               </div>
               <div className="ml-3">
                 <h3 className="text-lg font-semibold text-red-800">
@@ -164,94 +218,31 @@ export default function ContactFormTest() {
           className="grid grid-cols-1 mt-8 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
         >
           <div>
-            <label
-              htmlFor="firstName"
-              className="block text-base font-medium text-gray-700"
-            >
-              First name
-            </label>
-            <div className="relative mt-1">
-              <input
-                className={`py-3 px-4 block w-full shadow-sm rounded-md ${
-                  errors.firstName
-                    ? `pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500`
-                    : 'focus:ring-blue-900 focus:border-blue-900 border-gray-300'
-                }`}
-                {...register('firstName', {
-                  required: 'First name is required'
-                })}
-                id="firstName"
-                type="text"
-              />
-
-              {errors.firstName && <FormErrorIcon />}
-            </div>
-            {errors.firstName && (
-              <p className="mt-2 text-sm text-red-600" id="firstName-error">
-                {errors.firstName.message}
-              </p>
-            )}
+            <Input
+              label="firstName"
+              type="text"
+              placeholder="First Name"
+              register={register}
+              labelText="First Name"
+            />
           </div>
           <div>
-            <label
-              htmlFor="lastName"
-              className="block text-base font-medium text-gray-700"
-            >
-              Last name
-            </label>
-            <div className="relative mt-1">
-              <input
-                className={`py-3 px-4 block w-full shadow-sm rounded-md ${
-                  errors.lastName
-                    ? `pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500`
-                    : 'focus:ring-blue-900 focus:border-blue-900 border-gray-300'
-                }`}
-                id="lastName"
-                {...register('lastName', { required: 'Last name is required' })}
-                type="text"
-              />
-              {errors.lastName && <FormErrorIcon />}
-            </div>
-            {errors.lastName && (
-              <p className="mt-2 text-sm text-red-600" id="lastName-error">
-                {errors.lastName.message}
-              </p>
-            )}
+            <Input
+              label="lastName"
+              type="text"
+              placeholder="Last Name"
+              register={register}
+              labelText="Last Name"
+            />
           </div>
-
           <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="block text-base font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <div className="relative mt-1">
-              <input
-                className={`py-3 px-4 block w-full shadow-sm rounded-md ${
-                  errors.email
-                    ? `pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500`
-                    : 'focus:ring-blue-900 focus:border-blue-900 border-gray-300'
-                }`}
-                id="email"
-                name="email"
-                {...register('email', {
-                  required: 'Please enter your email',
-                  pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                    message: 'Please enter a valid email address'
-                  }
-                })}
-                required
-                type="text"
-              />
-              {errors.email && <FormErrorIcon />}
-            </div>
-            {errors.email && (
-              <p className="mt-2 text-sm text-red-600" id="email-error">
-                {errors.email.message}
-              </p>
-            )}
+            <Input
+              label="email"
+              type="email"
+              placeholder="Email address"
+              register={register}
+              labelText="Email"
+            />
           </div>
           <div className="sm:col-span-2">
             <label
@@ -269,9 +260,7 @@ export default function ContactFormTest() {
                 }`}
                 id="message"
                 name="message"
-                {...register('message', {
-                  required: 'Please enter your message'
-                })}
+                {...register('message')}
                 required
                 rows={4}
               ></textarea>

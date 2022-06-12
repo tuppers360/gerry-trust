eef3f501faa958d0a1cc94fcdd049c5983251f0a
tuppers360 <tuppers360@live.co.uk>
2022-06-12 19:22:46 +0100

    remove unused components

--- components/FormErrorIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { ExclamationCircleIcon } from '@heroicons/react/solid';
-
-export default function FormErrorIcon() {
-  return (
-    <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-      <ExclamationCircleIcon className="w-5 h-5 text-red-500" />
-    </div>
-  );
-}
--- components/stories/StoriesContainer.tsx
+++
@@ -1,3 +0,0 @@
-export default function StoriesContainer({ children }) {
-  return <div className="container mx-auto px-5">{children}</div>;
-}

fefed4e84626530fe6be8b3f5f2b04f6f38ae9ac
tuppers360 <tuppers360@live.co.uk>
2022-06-12 21:28:12 +0100

    amend styling of header section for story post

--- pages/stories/[slug].tsx
+++ pages/stories/[slug].tsx
@@ -29,25 +29,16 @@ export default function StoryPage({ story }: { story: Story }) {
         <div className="inline-flex items-center p-2">
           {story.author && <h3>written by {story.author.name}</h3>}
         </div>
-        <div className="p-2 text-sm text-gray-500 text">
+        <div className="flex items-center justify-center p-2 space-x-2 text-sm text-gray-500 text">
+          <span className="inline-flex items-center">
+            <BookOpenIcon className="inline-block w-5 h-5 mr-2" />
+            {story.readingTime.text}
+          </span>
+          <span>{` • `}</span>
           <span className="inline-flex items-center">
             <EyeIcon className="w-5 h-5 mr-2" />
             <ViewCounter slug={story.slug} />
           </span>
-        </div>
-
-        <div className="flex justify-between mt-4 text-sm text-gray-500 text">
-          <div className="inline-flex items-center space-x-2">
-            <span className="inline-flex items-center">
-              <BookOpenIcon className="inline-block w-5 h-5 mr-2" />
-              {story.readingTime.text}
-            </span>
-            <span>{` • `}</span>
-            <span className="inline-flex items-center">
-              <EyeIcon className="w-5 h-5 mr-2" />
-              <ViewCounter slug={story.slug} />
-            </span>
-          </div>
         </div>
       </div>
       <article className="flex flex-col items-start justify-center max-w-xl px-4 mx-auto mt-8 sm:px-6 lg:max-w-5xl lg:px-8">

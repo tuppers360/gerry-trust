ab3acb40a8ea118e0349956decfd87d9efd74565
tuppers360 <tuppers360@live.co.uk>
2022-06-18 11:44:42 +0100

    amend gift aid text and declaration

--- components/GiftAid.tsx
+++ components/GiftAid.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 type Donation = {
   donation: number;
   giftaid: boolean;
@@ -38,31 +40,62 @@ export default function GiftAid({
           current year. Your address is needed to identify you as a current UK
           taxpayer.
         </p>
-        <label className="mt-4 flex flex-col items-center">
-          <input
-            type="checkbox"
-            className="h-6 w-6 rounded border-2 text-blue-800 transition-colors duration-300 ease-in-out focus:ring-0 focus:ring-offset-0"
-            onClick={() => handleGiftAid()}
-          />
-          <span className="text-md mt-4 font-semibold">
-            Please claim Gift Aid on my behalf
-          </span>
-        </label>
+        <div className="relative mt-6 flex w-full items-center rounded-lg bg-slate-200/60 p-6 text-left dark:bg-slate-800">
+          <div className="flex h-5 items-center">
+            <input
+              id="giftaid"
+              aria-describedby="giftaid-statement"
+              name="giftaid"
+              type="checkbox"
+              className="h-6 w-6 rounded border-2 text-blue-800 transition-colors duration-300 ease-in-out focus:ring-0 focus:ring-offset-0"
+              onClick={() => handleGiftAid()}
+            />
+          </div>
+          <div className="ml-4 text-sm">
+            <label htmlFor="giftaid-statement" className="text-base font-bold">
+              Yes, I am a UK taxpayer and would like to Gift Aid my donations to
+              the Gerry Richardson Trust, past, present and future.
+            </label>
+          </div>
+        </div>
       </div>
 
       {giftaid && (
-        <div className="mt-10 rounded-lg border bg-slate-200/60 p-4 text-center leading-loose dark:bg-slate-800">
+        <div className="mt-10 space-y-5 rounded-lg bg-slate-200/60 p-4 leading-loose dark:bg-slate-800">
           <p>
             I confirm that this is my own money and I would like The Gerry
             Richardson Trust to treat all the donations I have made in the past
             4 years (if any) and any future donations I make, unless I notify
             you otherwise, as Gift Aid donations.
           </p>
-          <p className="mt-2">
-            I also confirm that I am a UK taxpayer and understand that if I pay
-            less Income Tax and/or Capital Gains Tax in the current tax year
-            than the amount of Gift Aid claimed on all my donations it is my
-            responsibility to pay any difference.
+          <p>
+            I am a UK taxpayer and understand that if I pay less Income Tax
+            and/or Capital Gains Tax than the amount of Gift Aid claimed on all
+            my donations in that tax year it is my responsibility to pay any
+            difference
+          </p>
+          <p>
+            Please notify the charity if you:
+            <ul>
+              <li>want to cancel this declaration</li>
+              <li>change your name or home address</li>
+              <li>
+                no longer pay sufficient tax on your income and/or capital gains
+              </li>
+            </ul>
+          </p>
+          <p>
+            If you pay Income Tax at the higher or additional rate and want to
+            receive the additional tax relief due to you, you must include all
+            your Gift Aid donations on your Self-Assessment tax return or ask HM
+            Revenue and Customs to adjust your tax code. For more information
+            about Gift Aid please take a look at the
+            <Link href="https://www.gov.uk/government/organisations/hm-revenue-customs">
+              <a className="ml-1 font-semibold text-sky-700 no-underline hover:text-sky-500 hover:underline dark:text-sky-300 dark:hover:text-sky-500">
+                HMRC website
+              </a>
+            </Link>
+            .
           </p>
         </div>
       )}

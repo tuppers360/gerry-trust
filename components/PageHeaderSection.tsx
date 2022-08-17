7b1142795a2a945817e437d1b83f9228462a564c
tuppers360 <tuppers360@live.co.uk>
2022-08-17 21:48:41 +0100

    amend formatting

--- components/PageHeaderSection.tsx
+++ components/PageHeaderSection.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 type HeaderProps = {
   title?: string;
   heading: string;
@@ -10,7 +8,7 @@ type HeaderProps = {
 
 const PageHeaderSection = ({ title, heading, children }: HeaderProps) => {
   return (
-    <div className="pt-12 pb-12">
+    <div className="pt-12 pb-6">
       <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-3xl lg:px-8">
         {title && (
           <p className="text-xl font-semibold uppercase tracking-wider text-sky-600">
